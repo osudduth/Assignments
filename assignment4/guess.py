2b484d5d3f1e5aa24668937e5c0935a3ac074061
Master Oliver <osudduth@gmail.com>
2015-01-02 19:30:04 -0800

    started on guess

---
+++ assignment4/guess.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+
+n=5
+userNumber = input('Choose a number between 1 and 100: ')
+g=userNumber
+userNumber=int(g)
+if n == userNumber: 
+	print ("You guessed it!")
+else: 
+	print ("you are wrong, loser!")
